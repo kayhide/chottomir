7bb3b84946d380e2a71a3b8a61a28cc383a54336
kayhide <kayhide@gmail.com>
2019-11-22 14:12:28 +0900

    Create dummy users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+# coding: utf-8
 class User < ApplicationRecord
+
+  def self.pinoko
+    find_by(
+      email: "pinoko@chottomir.test",
+    )
+  end
+
+  def self.kiriko
+    find_by(
+      email: "kiriko@chottomir.test",
+    )
+  end
 end
